21899829fcc630a64409f202fa1cc528da6bc7be
VladIvanov2001 <45943204+VladIvanov2001@users.noreply.github.com>
2020-11-09 15:50:05 +0300

    Update index.js

--- springPage/index.js
+++ springPage/index.js
@@ -38,48 +38,65 @@ arrDescription[5] = {
     image: 'images/spring-security.svg'
 };
 
+
+function returnArr(arr, string) {
+    let newArr = [];
+    arr.forEach((elem, index) => {
+        if (elem.name.toLowerCase().includes(string.toLowerCase()) || elem.description.toLowerCase().includes(string.toLowerCase())) {
+            newArr.push(elem);
+        }
+    });
+    return newArr;
+}
+
 const inputForSearch = document.querySelector('.search');
 const documentForBlocks = document.querySelector('.content__info');
 const attention = document.createElement('p');
 
 attention.innerHTML = 'There is no result';
 
+function renderArr(arr){
+    arr.forEach((elem, index) => {
+        const newItem = document.createElement('a');
+        newItem.classList.add('content__info__item');
+        const newPicture = document.createElement('div');
+        newPicture.classList.add('content__info__item-picture');
+        const newDescription = document.createElement('div');
+        newDescription.classList.add('content__info__item-description');
+        const headerForItem = document.createElement('h3');
+        const descriptionForItem = document.createElement('p');
+        const imageForItem = document.createElement('img');
+        imageForItem.src = elem.image;
+        headerForItem.innerHTML = elem.name;
+        descriptionForItem.innerHTML = elem.description;
+        newPicture.append(imageForItem);
+        newDescription.append(headerForItem);
+        newDescription.append(descriptionForItem);
+        newItem.append(newPicture);
+        newItem.append(newDescription);
+        documentForBlocks.append(newItem);
+    });
+}
 
-arrDescription.forEach((elem, index) => {
-    const newItem = document.createElement('a');
-    newItem.classList.add('content__info__item');
-    const newPicture = document.createElement('div');
-    newPicture.classList.add('content__info__item-picture');
-    const newDescription = document.createElement('div');
-    newDescription.classList.add('content__info__item-description');
-    const headerForItem = document.createElement('h3');
-    const descriptionForItem = document.createElement('p');
-    const imageForItem = document.createElement('img');
-    imageForItem.src = elem.image;
-    headerForItem.innerHTML = elem.name;
-    descriptionForItem.innerHTML = elem.description;
-    newPicture.append(imageForItem);
-    newDescription.append(headerForItem);
-    newDescription.append(descriptionForItem);
-    newItem.append(newPicture);
-    newItem.append(newDescription);
-    documentForBlocks.append(newItem);
-});
+renderArr(arrDescription);
 
-const infoBlock = document.querySelectorAll('.content__info__item');
 inputForSearch.addEventListener('keyup', () => {
-    let blockCounter = 0;
-    arrDescription.forEach((elem, index) => {
-        if (elem.name.toLowerCase().includes(inputForSearch.value.toLowerCase()) || elem.description.toLowerCase().includes(inputForSearch.value.toLowerCase())) {
-            infoBlock[index].style.display = 'block';
-        } else {
-            infoBlock[index].style.display = 'none';
-            blockCounter++;
-            if (blockCounter === arrDescription.length) {
-                documentForBlocks.append(attention);
-            } else {
-                attention.remove();
-            }
-        }
-    })
+    while (documentForBlocks.firstChild) {
+        documentForBlocks.removeChild(documentForBlocks.firstChild);
+    }
+    let finalResult = returnArr(arrDescription, inputForSearch.value);
+    if (finalResult.length === 0) {
+        documentForBlocks.append(attention);
+    } else {
+        renderArr(finalResult)
+    }
 });
+
+const obj = {
+    name: 'Spring Data',
+    description: 'Provides a consistent approach to data access relational,\n' +
+        '                            non-relational, map-reduce, and beyond.',
+    image: 'images/spring-data.svg'
+};
+
+arrDescription.push(obj);

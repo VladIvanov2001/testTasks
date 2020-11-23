3fd29d28a7647d12b7819068551ca555f408f621
VladIvanov2001 <45943204+VladIvanov2001@users.noreply.github.com>
2020-11-23 12:15:24 +0300

    add: message about mistake

---
+++ react/spring-page/src/components/Description/Description.js
@@ -0,0 +1,18 @@
+import React from "react";
+import './Description.css'
+
+function Description() {
+    return(
+        <div className="container description">
+            <h1 className="header-title">Projects</h1>
+            <p className="big-text">From configuration to&nbsp;security, web apps to&nbsp;big data&mdash;whatever
+                the
+                infrastructure needs of&nbsp;your
+                application may&nbsp;be, there is&nbsp;a&nbsp;Spring Project to&nbsp;help you build&nbsp;it. Start
+                small
+                and use just what you need&mdash;Spring is&nbsp;modular by&nbsp;design.</p>
+        </div>
+    )
+}
+
+export default Description
--- react/spring-page/src/components/Main/Main.js
+++ react/spring-page/src/components/Main/Main.js
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import './Main.css'
-import * as ArrayInfo from'../../constants/SpringInfo'
+import * as ArrayInfo from '../../constants/SpringInfo'
 import springBoot from '../../images/spring-boot.svg'
+import Description from "../Description/Description";
 
 
 function returnArr(arr, string) {
@@ -17,40 +18,37 @@ function returnArr(arr, string) {
 function Main() {
     const [name, setName] = useState('');
     const newArr = returnArr(ArrayInfo.arrDescription, name);
-    return(
+
+    return (
         <main className="main">
-            <div className="container description">
-                <h1 className="header-title">Projects</h1>
-                <p className="big-text">From configuration to&nbsp;security, web apps to&nbsp;big data&mdash;whatever
-                    the
-                    infrastructure needs of&nbsp;your
-                    application may&nbsp;be, there is&nbsp;a&nbsp;Spring Project to&nbsp;help you build&nbsp;it. Start
-                    small
-                    and use just what you need&mdash;Spring is&nbsp;modular by&nbsp;design.</p>
-            </div>
+            <Description/>
             <div className="content">
                 <label className="custom-input">
-                    <input type="text" className="search" placeholder="Input your words" value={name} onChange={e => setName(e.target.value)}/>
+                    <input type="text" className="search" placeholder="Input your words" value={name}
+                           onChange={e => setName(e.target.value)}/>
                 </label>
                 <div className="container">
                     <div className="content__info">
-                        {newArr.map((item) =>{
-                            return(
-                                <a className='content__info__item'>
-                                    <div className='content__info__item-picture'>
-                                        <img src={item.image}/>
-                                    </div>
-                                    <div className='content__info__item-description'>
-                                        <h3>{item.name}</h3>
-                                        <p>{item.description}</p>
-                                    </div>
-                                </a>
-                            )
-                        })}
+                        { newArr.length !== 0 ?(
+                            newArr.map((item) => {
+                                return (
+                                    <a className='content__info__item'>
+                                        <div className='content__info__item-picture'>
+                                            <img alt="image" src={item.image}/>
+                                        </div>
+                                        <div className="content__info__item-description">
+                                            <h3>{item.name}</h3>
+                                            <p>{item.description}</p>
+                                        </div>
+                                    </a>
+                                )
+                            })) : <p>There is no result</p>
+                        }
                     </div>
                 </div>
             </div>
         </main>
     )
 }
+
 export default Main

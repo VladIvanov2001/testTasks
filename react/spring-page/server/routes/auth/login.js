7840c603e06d930675dfd5a788ca14cfb1d06c08
VladIvanov2001 <45943204+VladIvanov2001@users.noreply.github.com>
2021-01-16 02:23:33 +0300

    fix: var name and filter's place

--- react/spring-page/server/routes/auth/login.js
+++ react/spring-page/server/routes/auth/login.js
@@ -8,14 +8,14 @@ const User = require('../../models/User');
 router.post('/signup', async function (request, response, next) {
     const {user} = request.body;
     try {
-        const resultForDB = await User.findOne({
+        const necessaryUser = await User.findOne({
             raw: true,
             where: {
                 name: user.username,
             },
             attributes: ['name', 'password']
         });
-        if (resultForDB.name && await bcrypt.compare(user.password, resultForDB.password)) {
+        if (necessaryUser.name && await bcrypt.compare(user.password, necessaryUser.password)) {
             const token = jwt.sign({
                 login: user.username,
             }, keys.jwt, {expiresIn: 60 * 60});
--- react/spring-page/server/routes/info/infoAboutTypes.js
+++ react/spring-page/server/routes/info/infoAboutTypes.js
@@ -1,28 +1,21 @@
 require('dotenv').config();
 const express = require('express');
 const router = express.Router();
-const sequelize = require('../../sequelize');
 const InfoAboutSpring = require('../../models/InfoAboutSpring');
-
-sequelize.sync().then(result => {
-    console.log('result');
-})
-    .catch(err => console.log(err));
 
 router.get('/spring', async (req, res) => {
     const nameForFilter = req.query.filter;
     try {
         const result = await InfoAboutSpring.findAll({attributes: ['name', 'description', 'image']});
+        const filteredArr = result.filter((elem) => elem.name.toLowerCase()
+                .includes(nameForFilter.toLowerCase())
+            || elem.description.toLowerCase()
+                .includes(nameForFilter.toLowerCase()));
+        return res.json(filteredArr);
     }
     catch (e){
         next(e);
     }
-    const filtredArr = result.filter((elem) => elem.name.toLowerCase()
-            .includes(nameForFilter.toLowerCase())
-        || elem.description.toLowerCase()
-            .includes(nameForFilter.toLowerCase()));
-
-    return res.json(filtredArr);
 });
 
 module.exports = router;
